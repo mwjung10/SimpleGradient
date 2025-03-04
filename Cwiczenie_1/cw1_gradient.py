3f2a2f6a4eff9a8aee8a47b6d992ad07dc6d5ed0
mwjung <mwjung10@gmail.com>
2025-03-04 14:48:40 +0100

    Initial commit

---
+++ Cwiczenie_1/cw1_gradient.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def visualize_fun(obj_fun: callable, trajectory: np.ndarray):
+    min_x, min_y = trajectory[-1]
+    MIN_X = 10
+    MAX_X = 10
+    PLOT_STEP = 100
+
+    x1 = np.linspace(-MIN_X, MAX_X, PLOT_STEP)
+    x2 = np.linspace(-MIN_X, MAX_X, PLOT_STEP)
+    X1, X2 = np.meshgrid(x1, x2)
+    Z = obj_fun(X1, X2)
+
+    plt.figure(figsize=(8, 6))
+    plt.pcolormesh(X1, X2, Z, cmap='viridis', shading='auto')
+    plt.colorbar(label='Objective Function Value')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.title('Objective Function Visualization')
+
+    plt.scatter(min_x, min_y, color='yellow', label='Minimum found by gradient descent alg.')
+    plt.plot(trajectory[:, 0], trajectory[:, 1], marker='o', color='red', label='Gradient Descent Steps', alpha=0.5)
+
+    plt.legend()
+    plt.show()
